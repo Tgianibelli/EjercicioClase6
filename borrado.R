29eb55a4172a82264be4156a5866083b1f9504c8
Tgianibelli <t.j.gianibelli@gmail.com>
2020-10-19 23:48:08 -0300

    cambios

--- borrado.R
+++ borrado.R
@@ -20,5 +20,12 @@ muestra <- separate(muestra, calle, sep = "\\D+", into = c("3", "4"), remove = F
 muestra <- muestra[c("1","4","localidad","provincia")]
 muestra <- muestra %>% rename(calle = 1) %>% rename(número = 2)
 
+# Vuelvoo a usar separate para limpiar columna calle
 
- 
+muestra <- separate(muestra, calle, sep = "Sin datos|Sin Número|N°", into = c("1", "2"), remove = FALSE)
+
+# Nuevamente nos quedamos con columnas que me sirven + ordeno/nombro
+
+muestra <- muestra[c("1", "número", "localidad", "provincia")]
+muestra <- muestra %>% rename(calle = 1)
+
